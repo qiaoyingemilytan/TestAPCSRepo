13ffe744e0e3c227021cd4c7358d3b428f2f1381
Emily <qtan20@stuy.edu>
2020-10-21 16:54:13 -0400

    I tested this program and it works now

--- Demo.java
+++ Demo.java
@@ -17,4 +17,46 @@ public static String printLoop(int n){
   }
   return numbers;
 }
+
+public static String arrToString(int[] arr){
+  String newString = "{";
+  for(int i = 0; i < arr.length - 1; i++){
+    newString += arr[i];
+    newString += ", ";
+  }
+  if(arr.length > 0){
+    newString += (arr[arr.length-1]);
+  }
+  newString += "}";
+  return newString;
+}
+
+public static String arrayDeepToString(int[][] arr){
+  String newString = "{";
+  for(int i = 0; i < arr.length - 1; i++){
+    newString += arrToString(arr[i]);
+    newString += ", ";
+  }
+  if(arr.length > 0){
+    newString += arrToString(arr[arr.length-1]);
+  }
+  newString += "}";
+  return newString;
+}
+
+public static int[][] create2DArray(int rows, int cols, int maxValue){
+  int[][] arr = new int[rows][cols];
+  for(int i = 0; i < rows; i++){
+    for(int j = 0; j < cols; j++){
+      arr[i][j] = (int)(Math.random() * (maxValue));
+    }
+  }
+  return arr;
+}
+
+public static int[][] create2DArrayRandomized(int rows, int cols, int maxValues){
+  cols = (int)(Math.random() * (cols));
+  return create2DArray(rows, cols, maxValues);
+}
+
 }
